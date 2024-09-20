2c6b3f84ff8dd0adea23f8648d1787727592daec
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-09-20 16:35:44 -0300

    Project updates

---
+++ src/data/vector_map.jl
@@ -0,0 +1,46 @@
+@kwdef mutable struct MapData <: AbstractData
+    collection_to::String
+    id::String
+    data::Vector{Vector{Int}} = []
+end
+
+function Base.convert(::Type{MapData}, tuple::Tuple{String, String})
+    return MapData(collection_to = tuple[1], id = tuple[2])
+end
+
+function Base.getindex(parameter::MapData, i::Integer)
+    return parameter.data[i]
+end
+
+function Base.length(parameter::MapData)
+    return length(parameter.data)
+end
+
+function Base.isempty(parameter::MapData)
+    return isempty(parameter.data)
+end
+
+function Base.iterate(parameter::MapData)
+    return iterate(parameter.data)
+end
+
+function Base.iterate(parameter::PSRBridge.MapData, i::Integer)
+    return iterate(parameter.data, i)
+end
+
+function raw_data(parameter::MapData)
+    return parameter.data
+end
+
+function initialize!(parameter::MapData, collection::AbstractCollection, db::DatabaseSQLite; kwargs...)
+    parameter.data = PSRI.get_vector_map(inputs.db, collection.id, parameter.collection_to, parameter.id)
+    return nothing
+end
+
+function update!(parameter::MapData, collection::AbstractCollection, db::DatabaseSQLite; kwargs...)
+    return nothing
+end
+
+function finalize!(parameter::MapData)
+    return nothing
+end
